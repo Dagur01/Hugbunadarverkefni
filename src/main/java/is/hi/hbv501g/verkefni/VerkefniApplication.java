f1e04caf7a2caf82b4c94a0a3d7f58a9461c1b81
Dagur01 <dagurgudna@gmail.com>
2025-10-05 14:01:36 +0000

    deploy update

--- src/main/java/is/hi/hbv501g/verkefni/VerkefniApplication.java
+++ src/main/java/is/hi/hbv501g/verkefni/VerkefniApplication.java
@@ -2,15 +2,11 @@ package is.hi.hbv501g.verkefni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "is.hi.hbv501g.verkefni.persistence.repositories")
-@EntityScan(basePackages = "is.hi.hbv501g.verkefni.persistence.entities")
 public class VerkefniApplication {
 
 
